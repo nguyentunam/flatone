f7a406f4244030d36c1295479bf342aa9508c1df
namnt <nam@vn-nest.tech>
2019-10-29 21:06:05 +0700

    bg

--- footer.php
+++ footer.php
@@ -17,7 +17,8 @@
 		<div class="footer-header">
 			<div class="container">
 				<div class="row">
-
+					<span class="icon-global"></span>
+					<h3>VIỆC LÀM</h3>
 				</div>
 			</div><!-- .site-info -->
 		</div>
@@ -25,27 +26,126 @@
 			<div class="container">
 				<div class="row">
 					<div class="col-md-4">
-						Trụ sở 1
+						<h3>Trụ sở chính Hà Nội </h3>
+						<div class="info">
+							<span class="fa fa-home"></span>
+                            Lô 5+6, Khu công nghiệp Lai Xá, Kim Chung, H. Hoài Đức, TP.Hà Nội.                        
+						</div>
+						<div class="info">
+							<span class="fa fa-phone-square"></span>
+                            <span class='label'>Điện thoại:</span> 
+                            <span class='value'>0987654321</span>                   
+						</div>
+						<div class="info">
+							<span class="fa fa-envelope"></span>
+                            <span class='label'>Email:</span> 
+                            <span class='value'>abc@gmail.com</span>                   
+						</div>
+						<div class="info">
+							<span class="fa fa-map-marker"></span>
+                            <span class='label'>Email:</span> 
+                            <span class='value'><a href="#">Xem bản đồ</a></span>                   
+						</div>
 					</div>
 					<div class="col-md-4">
-						Trụ sở 2
+						<h3>Chi nhánh Đà Nẵng</h3>
+						<div class="info">
+							<span class="fa fa-home"></span>
+                            Lô 5+6, Khu công nghiệp Lai Xá, Kim Chung, H. Hoài Đức, TP.Hà Nội.                        
+						</div>
+						<div class="info">
+							<span class="fa fa-phone-square"></span>
+                            <span class='label'>Điện thoại:</span> 
+                            <span class='value'>0987654321</span>                   
+						</div>
+						<div class="info">
+							<span class="fa fa-envelope"></span>
+                            <span class='label'>Email:</span> 
+                            <span class='value'>abc@gmail.com</span>                   
+						</div>
+						<div class="info">
+							<span class="fa fa-map-marker"></span>
+                            <span class='label'>Email:</span> 
+                            <span class='value'><a href="#">Xem bản đồ</a></span>                   
+						</div>
 					</div>
 					<div class="col-md-4">
-						Trụ sở 3
+						<h3>Chi nhánh Hồ Chí Minh </h3>
+						<div class="info">
+							<span class="fa fa-home"></span>
+                            Lô 5+6, Khu công nghiệp Lai Xá, Kim Chung, H. Hoài Đức, TP.Hà Nội.                        
+						</div>
+						<div class="info">
+							<span class="fa fa-phone-square"></span>
+                            <span class='label'>Điện thoại:</span> 
+                            <span class='value'>0987654321</span>                   
+						</div>
+						<div class="info">
+							<span class="fa fa-envelope"></span>
+                            <span class='label'>Email:</span> 
+                            <span class='value'>abc@gmail.com</span>                   
+						</div>
+						<div class="info">
+							<span class="fa fa-map-marker"></span>
+                            <span class='label'>Email:</span> 
+                            <span class='value'><a href="#">Xem bản đồ</a></span>                   
+						</div>
+					</div>
+				</div>
+				<div class="panel-phone row">
+					<div class="col-md-3">
+						<h3>Thương hiệu</h3>
+						<div class="phone-number">
+							<img src="http://intechvietnam.com/imgs/layout/logoint.jpg" />
+						</div>
+					</div>
+					<div class="col-md-3">
+						<h3>Hotline Hà Nội</h3>
+						<div class="phone-number">
+							<span class="icon icon-zalo"></span>
+                            <span class='label'>Mr. Abc:</span> 
+                            <span class='value'>0987654321</span>   
+						</div>
+						<div class="phone-number">
+							<span class="icon icon-zalo"></span>
+                            <span class='label'>Mr. Abc:</span> 
+                            <span class='value'>0987654321</span>   
+						</div>
+					</div>
+					<div class="col-md-3">
+						<h3>Hotline Đà Nẵng</h3>
+						<div class="phone-number">
+							<span class="icon icon-zalo"></span>
+                            <span class='label'>Mr. Abc:</span> 
+                            <span class='value'>0987654321</span>   
+						</div>
+						<div class="phone-number">
+							<span class="icon icon-zalo"></span>
+                            <span class='label'>Mr. Abc:</span> 
+                            <span class='value'>0987654321</span>   
+						</div>
+					</div>
+					<div class="col-md-3">
+						<h3>Hotline Hồ Chí Minh</h3>
+						<div class="phone-number">
+							<span class="icon icon-zalo"></span>
+                            <span class='label'>Mr. Abc:</span> 
+                            <span class='value'>0987654321</span>   
+						</div>
+						<div class="phone-number">
+							<span class="icon icon-zalo"></span>
+                            <span class='label'>Mr. Abc:</span> 
+                            <span class='value'>0987654321</span>   
+						</div>
 					</div>
 				</div>
 				<div class="row">
-					<div class="col-md-3">
-						Brand
+					<div class="col-md-6">
+						<span class="icon icon-cc"></span>
+						Copyright © 2017 intechvietnam.com. All Rights Reserved            
 					</div>
-					<div class="col-md-3">
-						Hotline 1
-					</div>
-					<div class="col-md-3">
-						Hotline 2
-					</div>
-					<div class="col-md-3">
-						Hotline 3
+					<div class="col-md-6">
+						<span class="icon icon-verified"></span>          
 					</div>
 				</div>
 			</div><!-- .site-info -->
