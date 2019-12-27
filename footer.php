c41aa8c6e75203db68acf55e92991506a08fc19d
namnt <nguyentunam@gmail.com>
2019-12-27 22:38:42 +0700

    update footer

--- footer.php
+++ footer.php
@@ -26,68 +26,68 @@
 			<div class="container">
 				<div class="row">
 					<div class="col-md-4">
-						<h3>Trụ sở chính Hà Nội </h3>
+						<h3>Trụ sở chính TP.Hồ Chí Minh </h3>
 						<div class="info">
 							<span class="fa fa-home"></span>
-                            Lô 5+6, Khu công nghiệp Lai Xá, Kim Chung, H. Hoài Đức, TP.Hà Nội.                        
+                           618/13/8F Quang Trung, Gò Vấp, TP.Hồ Chí Minh 
 						</div>
 						<div class="info">
 							<span class="fa fa-phone-square"></span>
                             <span class='label'>Điện thoại:</span> 
-                            <span class='value'>0987654321</span>                   
+                            <span class='value'>0987.836.119</span>                   
 						</div>
 						<div class="info">
 							<span class="fa fa-envelope"></span>
                             <span class='label'>Email:</span> 
-                            <span class='value'>abc@gmail.com</span>                   
+                            <span class='value'>info@bangtaihoangminh.com</span>                   
 						</div>
 						<div class="info">
 							<span class="fa fa-map-marker"></span>
-                            <span class='label'>Email:</span> 
+                            <span class='label'>Vị trí:</span> 
                             <span class='value'><a href="#">Xem bản đồ</a></span>                   
 						</div>
 					</div>
 					<div class="col-md-4">
-						<h3>Chi nhánh Đà Nẵng</h3>
+						<h3>Chi nhánh TP.Cần Thơ</h3>
 						<div class="info">
 							<span class="fa fa-home"></span>
-                            Lô 5+6, Khu công nghiệp Lai Xá, Kim Chung, H. Hoài Đức, TP.Hà Nội.                        
+                           21/15 TL 918, P.Bình Thủy, Q.Bình Thủy, TP.Cần Thơ 
 						</div>
 						<div class="info">
 							<span class="fa fa-phone-square"></span>
                             <span class='label'>Điện thoại:</span> 
-                            <span class='value'>0987654321</span>                   
+                            <span class='value'>0987.836.119</span>                   
 						</div>
 						<div class="info">
 							<span class="fa fa-envelope"></span>
                             <span class='label'>Email:</span> 
-                            <span class='value'>abc@gmail.com</span>                   
+                            <span class='value'>info@bangtaihoangminh.com</span>                   
 						</div>
 						<div class="info">
 							<span class="fa fa-map-marker"></span>
-                            <span class='label'>Email:</span> 
+                            <span class='label'>Vị trí:</span> 
                             <span class='value'><a href="#">Xem bản đồ</a></span>                   
 						</div>
 					</div>
 					<div class="col-md-4">
-						<h3>Chi nhánh Hồ Chí Minh </h3>
+						<h3>Chi nhánh TP.Hà Nội</h3>
 						<div class="info">
 							<span class="fa fa-home"></span>
-                            Lô 5+6, Khu công nghiệp Lai Xá, Kim Chung, H. Hoài Đức, TP.Hà Nội.                        
+              Lô L2D, Khu tái định cư X2B, Hoàng Mai, Hà Nội.             
 						</div>
 						<div class="info">
 							<span class="fa fa-phone-square"></span>
                             <span class='label'>Điện thoại:</span> 
-                            <span class='value'>0987654321</span>                   
+                            <span class='value'>0987.836.119</span>                   
 						</div>
 						<div class="info">
 							<span class="fa fa-envelope"></span>
                             <span class='label'>Email:</span> 
-                            <span class='value'>abc@gmail.com</span>                   
+                            <span class='value'>info@bangtaihoangminh.com</span>                   
 						</div>
 						<div class="info">
 							<span class="fa fa-map-marker"></span>
-                            <span class='label'>Email:</span> 
+                            <span class='label'>Vị trí:</span> 
                             <span class='value'><a href="#">Xem bản đồ</a></span>                   
 						</div>
 					</div>
@@ -109,39 +109,24 @@
 						<h3>Hotline Hà Nội</h3>
 						<div class="phone-number">
 							<span class="icon icon-zalo"></span>
-                            <span class='label'>Mr. Abc:</span> 
-                            <span class='value'>0987654321</span>   
-						</div>
-						<div class="phone-number">
-							<span class="icon icon-zalo"></span>
-                            <span class='label'>Mr. Abc:</span> 
-                            <span class='value'>0987654321</span>   
+                            <span class='label'>Mr. Du:</span> 
+                            <span class='value'>0987.836.119</span>   
 						</div>
 					</div>
 					<div class="col-md-3">
 						<h3>Hotline Đà Nẵng</h3>
 						<div class="phone-number">
 							<span class="icon icon-zalo"></span>
-                            <span class='label'>Mr. Abc:</span> 
-                            <span class='value'>0987654321</span>   
-						</div>
-						<div class="phone-number">
-							<span class="icon icon-zalo"></span>
-                            <span class='label'>Mr. Abc:</span> 
-                            <span class='value'>0987654321</span>   
+                            <span class='label'>Mr. Sơn:</span> 
+                            <span class='value'>0914.583.553</span>   
 						</div>
 					</div>
 					<div class="col-md-3">
 						<h3>Hotline Hồ Chí Minh</h3>
 						<div class="phone-number">
 							<span class="icon icon-zalo"></span>
-                            <span class='label'>Mr. Abc:</span> 
-                            <span class='value'>0987654321</span>   
-						</div>
-						<div class="phone-number">
-							<span class="icon icon-zalo"></span>
-                            <span class='label'>Mr. Abc:</span> 
-                            <span class='value'>0987654321</span>   
+                            <span class='label'>Mr. Sơn:</span> 
+                            <span class='value'>0914.583.553</span>   
 						</div>
 					</div>
 				</div>
